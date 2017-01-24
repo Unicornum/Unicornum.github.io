fc200de08d6516198f11b0e35be8ec448fc032bc
Dmitry <CTAPOBEP@mail.ru>
2017-01-24 18:13:04 +0300

    Обновление сайта.

--- Main/menudata.js
+++ Main/menudata.js
@@ -1,5 +1,5 @@
 var menudata={children:[
 {text:"Главная",url:"index.html"},
-{text:"Релизы",url:"a00237.html"},
-{text:"Документация",url:"a00238.html"},
-{text:"Обо мне",url:"a00239.html"}]}
+{text:"Релизы",url:"a00245.html"},
+{text:"Документация",url:"a00246.html"},
+{text:"Обо мне",url:"a00247.html"}]}
