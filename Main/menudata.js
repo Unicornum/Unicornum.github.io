5004caf6eff1b47da5a0b2b9e2d63dab37c88507
Dmitry <CTAPOBEP@mail.ru>
2017-04-01 11:11:20 +0300

    [UnicornumVerum] Обновление сайта.

--- Main/menudata.js
+++ Main/menudata.js
@@ -1,5 +1,5 @@
 var menudata={children:[
 {text:"Главная",url:"index.html"},
-{text:"Релизы",url:"a00245.html"},
-{text:"Документация",url:"a00246.html"},
-{text:"Обо мне",url:"a00247.html"}]}
+{text:"Релизы",url:"a00230.html"},
+{text:"Документация",url:"a00231.html"},
+{text:"Обо мне",url:"a00232.html"}]}
