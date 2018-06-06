e551ad5832867d3b717d3934c93330771159b620
Dmitry <CTAPOBEP@mail.ru>
2018-06-06 11:46:57 +0300

    [Documentation] Update documentation (v2.1.0).

--- Externals.Documentation/search/all_3.js
+++ Externals.Documentation/search/all_3.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['doit',['DoIt',['../a30819.html#af9acde1e8ab36f88410fdc2f6f6789e5',1,'ClassName']]]
+  ['doit',['DoIt',['../a30813.html#af9acde1e8ab36f88410fdc2f6f6789e5',1,'ClassName']]]
 ];
--- Externals.Documentation/search/all_4.js
+++ Externals.Documentation/search/all_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['manual',['Manual',['../a30834.html',1,'index']]]
+  ['manual',['Manual',['../a30828.html',1,'index']]]
 ];
